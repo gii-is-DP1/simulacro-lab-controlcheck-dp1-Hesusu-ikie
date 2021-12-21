5cb08f48d6d3f1c252e47d1eda3e34338a9203f6
Hesusu-ikie <jesuspaezpaez@gmail.com>
2021-12-21 12:08:41 +0100

    Simulacro

--- src/main/java/org/springframework/samples/petclinic/product/ProductController.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductController.java
@@ -1,5 +1,18 @@
 package org.springframework.samples.petclinic.product;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/product")
 public class ProductController {
+	
+	@Autowired
+	private ProductService productService;
+	
+	
     
 }
